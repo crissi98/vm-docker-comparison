0746809e57df6e963867d016028c7610c7a1a62f
Christian Radow <christian.radow@online.de>
2021-01-04 11:16:08 +0100

    added cpu and memory limits for docker

--- helloWorldStartupTest/testScripts/testHelloStartupDocker.sh
+++ helloWorldStartupTest/testScripts/testHelloStartupDocker.sh
@@ -1,4 +1,4 @@
-(docker run -p 8080:8080 hello)&
+(docker run -p 8080:8080 --cpus 1 -m 2G hello)&
 curl http://localhost:8080/hello
 res=$?
 while test $res != 0
--- primeNumbersPerformanceTest/testDockerPerformance.sh
+++ primeNumbersPerformanceTest/testDockerPerformance.sh
@@ -1,4 +1,4 @@
-(docker run -p 8080:8080 primes)&
+(docker run -p 8080:8080 --cpus 1 -m 2G primes)&
 sleep 1s
 hyperfine "curl http://localhost:8080/primes/100000" --runs 10
 docker container kill "$(docker ps -q)"
